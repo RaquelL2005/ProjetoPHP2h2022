c7c5f94ec937441e072ae475bb4154d5d9ee3885
Raquell2005 <raqdezotti@gmail.com>
2022-08-08 20:18:32 -0300

    segundo teste em PHP

--- aula1.php
+++ aula1.php
@@ -1,5 +1,28 @@
 <?php
 
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+<body>
+
+<?php
+
+echo "OLÁ MUNDO";
+
+
+?>
+
+</body>
+</html>
+
+
+<?php
+
 echo "OLÁ MUNDO";
 
 
