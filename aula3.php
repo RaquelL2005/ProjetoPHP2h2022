b8a854ec8fe36bd0e84bcc82ee93f02e6bb2ab41
Raquell2005 <raqdezotti@gmail.com>
2022-08-15 20:22:25 -0300

    aula3

---
+++ aula3.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html>
+<?php 
+#Testando comentário
+#OIIIIIIIIII
+ ?>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>PHP - TESTANDO COMENTÁRIO</title>
+</head>
+<body>
+    <?php 
+    /*ESTES SÃO COMENTÁRIOS DE MUITAS LINHAS
+	😎🌹 TOMA
+    */
+    echo "Segundo Exemplo de PHP";
+    $data ="20 de agosto de 2003";  
+    //  Este é a apenas um comentário de linha xiuuuuu
+    ?>
+    <p>HTML</p>
+    Arquivo criado em<?php  echo $data ?>
+</body>
+</html>
+<?php 
+	
+?>
