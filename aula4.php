757bb7a04fc732960ba0918fc52c1b185c24122f
Raquell2005 <raqdezotti@gmail.com>
2022-08-17 20:25:27 -0300

    AULA4

---
+++ aula4.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Aula 4</title>
+</head>
+<body>
+
+<?php 
+print(" usando o print")
+$data ="20 de Agosto de 2020"; //string
+$salario = 850.00; //float ou double
+$cargo = "Estagiario "; //string
+$idade = 18; //integer ou int
+$resultado = true; //boolean
+
+if ($resultado) {
+	echo "Verdadeiro";
+	
+}
+
+
+ ?>
+</body>
+</html>
+
+
