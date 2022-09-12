7840e2561feedd312b71900ea5fc43ca560a182c
Raquell2005 <raqdezotti@gmail.com>
2022-09-12 20:21:32 -0300

    aula10

---
+++ aula10.php
@@ -0,0 +1,21 @@
+<?php 
+
+function validaNome(){
+echo "Executando!";
+
+}
+ function somaValor($num1,$num2){
+ return($num1+$num2);
+
+ }
+ 
+ //executando função
+
+ $valor1=20.0;
+ $valor2=1;
+
+validaNome();
+
+echo "<br/> A soma é ".somaValor($valor1,$valor2);
+
+ ?>
